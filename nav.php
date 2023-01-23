d23fb99a5d9aede3c30a19d37f52a8126414f6fe
adam waheed <adamwaheed@gmail.com>
2023-01-23 21:32:31 +0500

    basic Loop

--- nav.php
+++ nav.php
@@ -16,6 +16,10 @@
         <li class="nav-item">
           <a class="nav-link" href="conditions.php">Condition</a>
         </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="loop.php">Loop</a>
+        </li>
        
       </ul>
       <form class="d-flex" role="search">
